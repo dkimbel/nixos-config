ba4e0d8540ca82d78137cf3211978abaa1f1abf1
Daniel Kimbel <dkkimbel@gmail.com>
2023-11-23 11:14:45 -0700

    Add ssh agent configs

--- darwin/home-manager.nix
+++ darwin/home-manager.nix
@@ -115,6 +115,12 @@ in
         };
         ssh = {
           enable = true;
+          extraConfig = ''
+            Host github.com
+              AddKeysToAgent yes
+              UseKeychain yes
+              IdentityFile ~/.ssh/id_ed25519
+          '';
         };
       };
     };
