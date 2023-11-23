da245e33dc6bbf49fb7a94a81670cbc73bad1574
Daniel Kimbel <dkkimbel@gmail.com>
2023-11-23 11:42:12 -0700

    Try without macOS-specific ssh setting

--- darwin/home-manager.nix
+++ darwin/home-manager.nix
@@ -118,7 +118,6 @@ in
           extraConfig = ''
             Host github.com
               AddKeysToAgent yes
-              UseKeychain yes
               IdentityFile ~/.ssh/id_ed25519
           '';
         };
