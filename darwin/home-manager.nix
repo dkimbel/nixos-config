a68cd8c6c6d1b9eace9029d3e3ce92bf07e57b09
Daniel Kimbel <dkkimbel@gmail.com>
2023-11-24 22:18:53 -0700

    Get nerd font symbols and configure Starship further

--- darwin/home-manager.nix
+++ darwin/home-manager.nix
@@ -25,7 +25,8 @@ in
     "appcleaner"
     "bitwarden"
     "firefox"
-    "font-sf-mono-for-powerline"
+    "font-sf-mono"
+    "font-symbols-only-nerd-font"
     "obsidian"
   ];
 
@@ -124,6 +125,38 @@ in
 
         kitty = {
           enable = true;
+          extraConfig = ''
+            # Source of font patches:
+            # https://www.reddit.com/r/neovim/comments/vmizpo/comment/ie208xn
+            # https://github.com/kyza-2k/.dotfiles/blob/master/.config/kitty/symbols.conf
+            # "Nerd Fonts - Pomicons"
+            symbol_map  U+E000-U+E00D Symbols Nerd Font
+            # "Nerd Fonts - Powerline"
+            symbol_map U+e0a0-U+e0a2,U+e0b0-U+e0b3 Symbols Nerd Font
+            # "Nerd Fonts - Powerline Extra"
+            symbol_map U+e0a3-U+e0a3,U+e0b4-U+e0c8,U+e0cc-U+e0d2,U+e0d4-U+e0d4 Symbols Nerd Font
+            # "Nerd Fonts - Symbols original"
+            symbol_map U+e5fa-U+e62b Symbols Nerd Font
+            # "Nerd Fonts - Devicons"
+            symbol_map U+e700-U+e7c5 Symbols Nerd Font
+            # "Nerd Fonts - Font awesome"
+            symbol_map U+f000-U+f2e0 Symbols Nerd Font
+            # "Nerd Fonts - Font awesome extension"
+            symbol_map U+e200-U+e2a9 Symbols Nerd Font
+            # "Nerd Fonts - Octicons"
+            symbol_map U+f400-U+f4a8,U+2665-U+2665,U+26A1-U+26A1,U+f27c-U+f27c Symbols Nerd Font
+            symbol_map U+f44a Symbols Nerd Font
+            # "Nerd Fonts - Font Linux"
+            symbol_map U+F300-U+F313 Symbols Nerd Font
+            #  Nerd Fonts - Font Power Symbols"
+            symbol_map U+23fb-U+23fe,U+2b58-U+2b58 Symbols Nerd Font
+            #  "Nerd Fonts - Material Design Icons"
+            symbol_map U+f500-U+fd46 Symbols Nerd Font
+            # "Nerd Fonts - Weather Icons"
+            symbol_map U+e300-U+e3eb Symbols Nerd Font
+            # Misc Code Point Fixes
+            symbol_map U+21B5,U+25B8,U+2605,U+2630,U+2632,U+2714,U+E0A3,U+E615,U+E62B Symbols Nerd Font
+          '';
           font = {
             name = "SF Mono";
             size = 13.5;
@@ -156,6 +189,18 @@ in
           enableFishIntegration = true;
           settings = {
             add_newline = false;
+            format = "$all$time$line_break$jobs$battery$status$os$container$shell$character";
+            character = {
+              error_symbol = "[󰹆](bold red)";
+            };
+            git_branch = {
+              symbol = "";
+            };
+            time = {
+              disabled = false;
+              time_format = "%-I:%M%P";
+              use_12hr = true;
+            };
           };
         };
 
